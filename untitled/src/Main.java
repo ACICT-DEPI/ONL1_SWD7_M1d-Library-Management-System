61563efe30e1a7d70ff968aa70a739dbc6b4675c
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-08-12 03:33:33 +0300

    lib class methods

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // split this main to admin and subscriber
         String str = "";
         Scanner sc = new Scanner(System.in);
+        System.out.print("Welcome to our library!\n");
         do {
             str = sc.nextLine();
-            System.out.println("Welcome to our library!");
         } while ("quit".compareTo(str) != 0);
     }
 }
--- untitled/src/library.java
+++ untitled/src/library.java
@@ -1,10 +1,47 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class library {
     // this class represents every branch
     private final int id;
-    public String name;
+    public String branchName;
 
+    // Constructor
     public library(int id) {
         this.id = id;
-        name = "";
+        this.branchName = "";
+    }
+
+    // Getter for the id
+    public int getId() {
+        return id;
+    }
+
+    // Getter for the name
+    public String getName() {
+        return branchName;
+    }
+
+    // Setter for the name
+    public void setName(String name) {
+        this.branchName = name;
+    }
+
+    // Method to save library data to a file
+    public void saveToFile() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("libs.txt", true))) {
+            writer.write(this.id + "," + this.branchName);
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving library data.");
+            e.printStackTrace();
+        }
+    }
+
+    // Override the toString method to give a string representation of the Library
+    @Override
+    public String toString() {
+        return "Library [ID: " + id + ", Name: " + branchName + "]";
     }
 }
