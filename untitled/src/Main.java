43d63855c02818d73974d934ff8aa8b1c7484126
Tasneem Abdeltawab <tasneemabdeltawab205@gmail.com>
2024-08-26 19:06:53 +0300

    prev changes

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // split this main to admin and subscriber
+        // split this main to admin and user
         String str = "";
         Scanner sc = new Scanner(System.in);
         System.out.print("Welcome to our library!\n");
